7e317dcab516c2493193c4d469e5ae0f34cfaed8
Jérémy <jguillot@fieldbox.ai>
2022-10-18 01:07:44 +0200

    [FEAT] First views with templates.

--- handi_a_dit/urls.py
+++ handi_a_dit/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from quiz import views
+
 urlpatterns = [
-        path('', include('quiz.urls')),
+        path('', views.index),
+        path('quiz/', include('quiz.urls')),
     path('admin/', admin.site.urls),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,4 +1,24 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
+
+from quiz.models import Question
 
 def index(request):
-    return HttpResponse("Bienvenue sur la page d'accueil du site Handi A Dit !")
+    latest_question_list = Question.objects.order_by('-created_at')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+        }
+    return render(request, 'quiz/index.html', context)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Oh mince... Ce quiz n'existe pas !")
+    return render(request, 'quiz/detail.html', {"question": question})
+
+def results(request, question_id):
+    return HttpResponse(f"Vous regardez les résultats de la question {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"Vous répondez à la question {question_id}")
